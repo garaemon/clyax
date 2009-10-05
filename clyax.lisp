0412679aecb0b8e5f40efa49799130c01417d41c
garaemon <garaemon@gmail.com>
2009-10-05 22:30:46 +0900

    update for nurikabe basic xwindow

--- clyax.lisp
+++ clyax.lisp
@@ -6,5 +6,22 @@
 
 (defpackage :clyax
   (:use #:common-lisp #:cffi)
+  (:export
+   #:XFlush
+   #:XOpenDisplay
+   #:XDefaultRootWindow
+   #:XDefaultScreen
+   ;; event masks
+   #:ExposureMask
+   #:ButtonPressMask
+   #:ButtonReleaseMask
+   #:Button1MotionMask
+   #:StructureNotifyMask
+   #:SubstructureNotifyMask
+   ;; window attibute
+   #:CWEventMask
+   #:CWColormap
+   #:CWBackPixmap
+   #:CWOverrideRedirect
+   )
   )
-
