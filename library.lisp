89b17a8629e789facd8e990a7c86f9c04fd72a2f
garaemon <garaemon@gmail.com>
2009-10-06 02:57:47 +0900

    add gl support

--- library.lisp
+++ library.lisp
@@ -6,13 +6,15 @@
 
 (in-package :clyax)
 
-(define-foreign-library X11
-  (:darwin "/usr/X11/lib/libX11.dylib"))
 (define-foreign-library _trap_gl
   (:darwin "/System/Library/Frameworks/OpenGL.framework/Versions/A/OpenGL"))
-(define-foreign-library gl
-  (:darwin "/opt/local/lib/libGL.dylib"))
+(define-foreign-library X11
+  (:darwin "/usr/X11/lib/libX11.dylib"))
+  ;;(:darwin "/opt/local/lib/libX11.dylib"))
 
+(define-foreign-library gl
+  ;;(:darwin "/opt/local/lib/libGL.dylib"))
+  (:darwin "/usr/X11/lib/libGL.dylib"))
+;;(use-foreign-library _trap_gl)
 (use-foreign-library X11)
-(use-foreign-library _trap_gl)
 (use-foreign-library gl)
